d436b754b131f1860c1fa8408f527097f76ce278
Fabian Garcia <fabigg2001@hotmail.com>
2023-02-16 22:01:19 -0500

    form products

--- src/products/tableProducts/CreateProducts.tsx
+++ src/products/tableProducts/CreateProducts.tsx
@@ -1,17 +1,24 @@
 import './createProducts.css'
 import FormProducts from '../formProducts/FormProducts'
-import TableComponent from '../tableComponent/tableComponent'
+import TableComponent from '../tableComponent/TableComponent'
 import { useState } from 'react'
+import { IProduct } from '../../interface/products.interface'
 
 function CreateProducts() {
-  const [first, setfirst] = useState([])
+  const [products, setProducts] = useState([] as IProduct[])
+  const [product, setProduct] = useState({} as IProduct)
+  const arrayProducts: IProduct[] = []
 
-  const handlerForm = (producto:any) => {
-      setfirst([])
+  const handlerForm = (product: IProduct, event: React.ChangeEvent<HTMLInputElement>) => {
+    setProduct(product)
+    arrayProducts.push(product)
+    setProducts(arrayProducts)
+    event.preventDefault()
   }
+
   return (
     <div className="main-table">
-      <FormProducts setProducto = {handlerForm}/>
+      <FormProducts setProducts = {() => handlerForm}/>
       <div className='container-table'>
         <div className='sub-container-table'>
           <TableComponent/>
